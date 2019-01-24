ee0157faabc8b1422f1a6575b04df58e70e1de3f
swathi-swa <36359971+swathi-swa@users.noreply.github.com>
2019-01-24 21:50:39 +0530

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class hashmap
+{
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+HashMap< Integer,String> sw=new HashMap<>();
+sw.put(01,"madhaiyan");
+sw.put(02,"selvi");
+sw.put(03,"akshaya");
+sw.put(04,"swathi");
+System.out.println(sw.get(01));
+}}
